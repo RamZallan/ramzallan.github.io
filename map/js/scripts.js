6b78dd8667b979cfa290fbf3d68fa2dc7b98b8f7
Ram Zallan <ram.zallan@ht-la.org>
2016-01-22 23:05:59 -0800

    Re-fixed states with only 1 Team (vs. 1 Teams)

--- map/js/scripts.js
+++ map/js/scripts.js
@@ -32,83 +32,84 @@ jQuery(document).ready(function () {
         selectedColor: '#4eb0e3',
         onLabelShow: function (event, label, code) {
             //            /* Hardcoding labels for states with only 1 Team (vs. "X Teams") */
-                        if (code == 'mt') {
-                            label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Montana</strong>" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Team" + "\n" + "<font style='font-weight: 900;'>0</font>" + " Regionals"); 
-                        }
-            //            if (code == 'ne') {
-            //                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Nebraska</strong>" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Team"); 
-            //            }if (code == 'ak') {
-            //                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Alaska</strong>" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Team"); 
-            //            }
+            if (code == 'mt') {
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Montana</strong>" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Team" + "\n" + "<font style='font-weight: 900;'>0</font>" + " Regionals");
+            }
+            if (code == 'ne') {
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Nebraska</strong>" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Team" + "\n" + "<font style='font-weight: 900;'>0</font>" + " Regionals");
+            }
+            if (code == 'ak') {
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Alaska</strong>" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Team" + "\n" + "<font style='font-weight: 900;'>0</font>" + " Regionals");
+            }
             if (code == 'pa') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Pennsylvania</strong>" + "\n" + "<font style='font-weight: 900;'>73</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Regional" + "\n" + "<font style='font-weight: 900;'>4</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>MAR</font>" + " District Championships");
             }
             if (code == 'mo') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Missouri</strong>" + "\n" + "<font style='font-weight: 900;'>70</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>1</font>" + " Regional" + "\n" + "<font style='font-weight: 900;'><em>FIRST®</em></font>" + " Championships");
             }
-            
+
             if (code == 'or') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Oregon</strong>" + "\n" + "<font style='font-weight: 900;'>43</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>3</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>PNW</font>" + " District Championships");
             }
-            
+
             if (code == 'wa') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Washington</strong>" + "\n" + "<font style='font-weight: 900;'>114</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>6</font>" + " District Events");
             }
-            
+
             if (code == 'mi') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Michigan</strong>" + "\n" + "<font style='font-weight: 900;'>412</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>21</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>FIM</font>" + " District Championships");
             }
-            
+
             if (code == 'in') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Indiana</strong>" + "\n" + "<font style='font-weight: 900;'>49</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>3</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>Indiana</font>" + " District Championships");
             }
-            
+
             if (code == 'ga') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Georgia</strong>" + "\n" + "<font style='font-weight: 900;'>65</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>4</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>Peachtree</font>" + " District Championships");
             }
-            
+
             if (code == 'nc') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>North Carolina</strong>" + "\n" + "<font style='font-weight: 900;'>52</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>4</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>NC</font>" + " District Championships");
             }
-            
+
             if (code == 'va') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Virginia</strong>" + "\n" + "<font style='font-weight: 900;'>78</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>4</font>" + " District Events");
             }
-            
+
             if (code == 'md') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Maryland</strong>" + "\n" + "<font style='font-weight: 900;'>41</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>3</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>Chesapeake</font>" + " District Championships");
             }
-            
+
             if (code == 'de') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Delaware</strong>" + "\n" + "<font style='font-weight: 900;'>4</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>0</font>" + " District Events");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Delaware</strong>" + "\n" + "<font style='font-weight: 900;'>4</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>0</font>" + " District Events");
             }
-            
+
             if (code == 'nj') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Delaware</strong>" + "\n" + "<font style='font-weight: 900;'>77</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>4</font>" + " District Events");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Delaware</strong>" + "\n" + "<font style='font-weight: 900;'>77</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>4</font>" + " District Events");
             }
-            
+
             if (code == 'ct') {
                 label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Connecticut</strong>" + "\n" + "<font style='font-weight: 900;'>48</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>2</font>" + " District Events" + "\n" + "<font style='font-weight: 900;'>NE</font>" + " District Championships");
             }
-            
+
             if (code == 'ri') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Rhode Island</strong>" + "\n" + "<font style='font-weight: 900;'>6</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>1</font>" + " District Event");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Rhode Island</strong>" + "\n" + "<font style='font-weight: 900;'>6</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>1</font>" + " District Event");
             }
-            
+
             if (code == 'ma') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Massachusetts</strong>" + "\n" + "<font style='font-weight: 900;'>68</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>f</font>" + " District Events");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Massachusetts</strong>" + "\n" + "<font style='font-weight: 900;'>68</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>f</font>" + " District Events");
             }
-            
+
             if (code == 'vt') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Vermont</strong>" + "\n" + "<font style='font-weight: 900;'>4</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>0</font>" + " District Events");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Vermont</strong>" + "\n" + "<font style='font-weight: 900;'>4</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>0</font>" + " District Events");
             }
-            
+
             if (code == 'nh') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>New Hampshire</strong>" + "\n" + "<font style='font-weight: 900;'>32</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>2</font>" + " District Events");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>New Hampshire</strong>" + "\n" + "<font style='font-weight: 900;'>32</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>2</font>" + " District Events");
             }
-            
+
             if (code == 'me') {
-                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Maine</strong>" + "\n" + "<font style='font-weight: 900;'>24</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>1</font>" + " District Event");    
+                label.html("<strong style='font-size: 14px; font-weight: 900; color: #4eb0e3;'>Maine</strong>" + "\n" + "<font style='font-weight: 900;'>24</font>" + " Teams" + "\n" + "<font style='font-weight: 900;'>1</font>" + " District Event");
             }
         },
         onRegionOver: function (event, code, region) {
